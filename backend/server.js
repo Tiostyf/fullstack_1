6796dc400ab0b83be74ddbeb911dc18e51973b04
arjunsinghthakur031@gmail.com <ajunsinghthakur031@gmail.com>
2025-07-08 14:03:00 +0530

    agin update

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import path from "path";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
-app.use(express.json());
-
-app.use("/api/products", productRoutes);
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+app.use(express.json());
 
+// API routes
+app.use("/api/products", productRoutes);
+
+// Production static serving
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // or build if CRA
   app.get("", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`✅ Server running on port ${PORT}`);
+});
